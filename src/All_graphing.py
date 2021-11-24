f87169490d45b3c93f9eac4bb402a0e0d38f7b2a
Donna 'Katie' McCullough <dmccull8@vols.utk.edu>
2021-11-24 13:01:10 -0500

    graphing of all reps

--- src/All_graphing.py
+++ src/All_graphing.py
@@ -34,7 +34,7 @@ from pylab import *
 
 df_all = pd.read_csv("/Users/dkm/Documents/Talmy_research/Zinser_and_Ben/Project_1_nutrient_additions/data/NH4_add.csv")
 
-print(df_all)
+#print(df_all)
 
 df_all['rep1'] = df_all['rep1'].fillna(value = 0.0) #filling Nans with 09.0 in 'rep1' column 
 df_all['rep2'] = df_all['rep2'].fillna(value = 0.0 )#filling Nans with 09.0 in 'rep2' column 
@@ -45,42 +45,41 @@ df_all = df_all.dropna(axis = 1)     #taking NaN columns off the end of df but h
 
 print(df_all)
 
+df_all = df_all.rename({'Time(days)':'times'}, axis=1)    #'renaming column to make it callable by 'times'
+
+
+
+######################################
+
+#  Graphing Data 
+
+#####################################
+
+plt.figure()
+plt.scatter(df_all['times'],df_all['rep1'], label = 'rep1')
+plt.scatter(df_all['times'],df_all['rep2'], label = 'rep2')
+plt.scatter(df_all['times'],df_all['rep3'], label = 'rep3')
+plt.scatter(df_all['times'],df_all['rep4'], label = 'rep4')
+plt.scatter(df_all['times'],df_all['rep5'], label = 'rep5')
+plt.scatter(df_all['times'],df_all['rep6'], label = 'rep6')
 
 
 
 
-
+#df_40 = df_all.loc[(df_all['treatment'] == "40.0"),::]    #not working 
 
 
 '''
 
-#slicing into arrays since column names aren't callable rn :/ 
-
 time = df_main.iloc[:,1]
 rep1 = df_main.iloc[:,2]
 rep2 = df_main.iloc[:,3]
-rep3 = df_main.iloc[:,4]
-rep4 = df_main.iloc[:,5]
-rep5 = df_main.iloc[:,6]
-rep6 = df_main.iloc[:,7]
-
-#Graphing
-
-plt.figure()
-
-plt.plot(time,rep1,label = 'rep1')
-plt.plot(time,rep2,label = 'rep2')
-plt.plot(time,rep3,label = 'rep3')
-plt.plot(time,rep4,label = 'rep4')
-plt.plot(time,rep5,label = 'rep5')
-plt.plot(time,rep6,label = 'rep6')
-
 
 plt.semilogy()
-
+'''
 
 plt.legend()
-plt.title('MIT9215 Transfers', fontsize = '22')
+plt.title('NH4 additions to MIT9215 Pro', fontsize = '22')
 plt.xlabel('Time',fontsize = '18')
 plt.legend(prop={"size":14})
 plt.ylabel('Cell Abundance (flow)',fontsize = '18')
@@ -91,7 +90,7 @@ plt.yticks(fontsize = 14)
 
 plt.show()
 
-'''
+
 
 
 
